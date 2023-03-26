71dc6bffb6bb5d38da8b9da8597049044f8cf179
Jamie <jmorgan3475@gmail.com>
2023-03-26 16:50:48 -0600

    passing value through props to square component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import './App.css'
 
 // TODO: make board expandable like 3x3, 4x4, 5x10
+
 export default function Board() {
   return (
     <>
-      {BoardRow(1)}
-      {BoardRow(4)}
-      {BoardRow(7)}
+      <div className="board-row">
+        <Square value='1' />
+        <Square value='2' />
+        <Square value='3' />
+      </div>
+      <div className="board-row">
+        <Square value='4' />
+        <Square value='5' />
+        <Square value='6' />
+      </div>
+      <div className="board-row">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
     </>
-  )
+  );
 }
 
-function BoardRow(num) {
-  return (
-    <div className="board-row">
-      {Square(num)}
-      {Square(num + 1)}
-      {Square(num + 2)}
-    </div>
-  )
+function Square({ value }) {
+  return <button className="square">{value}</button>
 }
 
-function Square(num) {
-  return <button className="square">{num}</button>
-}
+// export default function Board() {
+//   return (
+//     <>
+//       {BoardRow(1)}
+//       {BoardRow(4)}
+//       {BoardRow(7)}
+//     </>
+//   )
+// }
+
+// function BoardRow(num) {
+//   return (
+//     <div className="board-row">
+//       {Square(num)}
+//       {Square(num + 1)}
+//       {Square(num + 2)}
+//     </div>
+//   )
+// }
+
