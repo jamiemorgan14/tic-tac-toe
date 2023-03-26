69019619ce2273411223a6e342862bf874eb1f14
Jamie <jmorgan3475@gmail.com>
2023-03-26 17:58:02 -0600

    props passing down to squares from board

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
 import './App.css'
+import { useState } from 'react';
 
 // TODO: make board expandable like 3x3, 4x4, 5x10
-
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(square) {
+    const nextSquares = squares.slice();
+    nextSquares[square] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value='1' />
-        <Square value='2' />
-        <Square value='3' />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value='4' />
-        <Square value='5' />
-        <Square value='6' />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
 }
 
-function Square({ value }) {
-  return <button className="square">{value}</button>
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
-
-// export default function Board() {
-//   return (
-//     <>
-//       {BoardRow(1)}
-//       {BoardRow(4)}
-//       {BoardRow(7)}
-//     </>
-//   )
-// }
-
-// function BoardRow(num) {
-//   return (
-//     <div className="board-row">
-//       {Square(num)}
-//       {Square(num + 1)}
-//       {Square(num + 2)}
-//     </div>
-//   )
-// }
-
